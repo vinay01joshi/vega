54ee203a3c82116dde54bf047bbd9c25a08cecf0
Vinay Joshi <vinay01joshi@gmail.com>
2017-09-26 19:22:22 +0530

    Adding Features Api1

---
+++ Controllers/FeatureController.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using vega.Controllers.Resources;
+using vega.Models;
+using vega.Persistence;
+
+namespace vega.Controllers
+{
+    public class FeatureController : Controller
+    {
+        private readonly IMapper mapper;
+        private readonly VegaDbContext context;
+        public FeatureController(VegaDbContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        [HttpGet("/api/features")]
+        public async Task<IEnumerable<FeatureResource>> GetFeatures()
+        {
+            var features = await context.Features.ToListAsync();
+            return mapper.Map<List<Feature>, List<FeatureResource>>(features);
+        }
+    }
+}
