8f440421321b06b55bbddbc35ac6876b8fabe4a6
Vinay Joshi <vinay01joshi@gmail.com>
2017-09-30 13:21:59 +0530

    Implementing CRUD operation | Server side validation

--- Controllers/Resources/VehicleResource.cs
+++ Controllers/Resources/VehicleResource.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 using vega.Models;
 
 namespace vega.Controllers.Resources
@@ -10,6 +11,8 @@ namespace vega.Controllers.Resources
         public int Id { get; set; }
         public int ModelId { get; set; }     
         public bool IsRegistered { get; set; }
+
+        [Required]
         public ContactResource Contact { get; set; }    
         public ICollection<int> Features { get; set; }
 
