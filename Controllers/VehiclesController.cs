957bc81870bfa0fc8e9b2407dd9b9d1bbc74de7c
Vinay Joshi <vinay01joshi@gmail.com>
2017-09-30 16:11:35 +0530

    Refactoring Towards the Clean Architecture | Changing return type to Api's

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -32,7 +32,14 @@ namespace vega.Controllers
             context.Vehicles.Add(vehicle);
             await context.SaveChangesAsync();
 
-            var result = mapper.Map<Vehicle,SaveVehicleResource>(vehicle);
+            await context.Vehicles
+               .Include(v => v.Features)
+                   .ThenInclude(vf => vf.Feature)
+               .Include(v => v.Model)
+                   .ThenInclude(m => m.Make)
+               .SingleOrDefaultAsync(v => v.Id == vehicle.Id);
+                
+            var result = mapper.Map<Vehicle,VehicleResource>(vehicle);
             return Ok(result);
         }
 
@@ -42,7 +49,12 @@ namespace vega.Controllers
             if(!ModelState.IsValid)
                 return BadRequest(ModelState);                
                        
-            var vehicle = await context.Vehicles.Include(v => v.Features).SingleOrDefaultAsync( v=> v.Id == id);
+              var vehicle = await context.Vehicles
+                .Include(v => v.Features)
+                    .ThenInclude(vf => vf.Feature)
+                .Include(v => v.Model)
+                    .ThenInclude(m => m.Make)
+                .SingleOrDefaultAsync(v => v.Id == id);
 
              if(vehicle == null)
                 return NotFound();
@@ -52,7 +64,7 @@ namespace vega.Controllers
 
             await context.SaveChangesAsync();
 
-            var result = mapper.Map<Vehicle,SaveVehicleResource>(vehicle);
+            var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
 
             return Ok(result);
         }
