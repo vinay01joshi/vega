7fe57880b3b34782dc8c3e1137e05f669e3b2a18
Vinay Joshi <vinay01joshi@gmail.com>
2017-09-30 15:58:36 +0530

    Refactoring Towards the Clean Architecture | Using Key value pair resource

--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -12,8 +12,9 @@ namespace vega.Mapping
         {
             // Domain to Api Resources
             CreateMap<Make, MakeResource>();
-            CreateMap<Model, ModelResource>();
-            CreateMap<Feature, FeatureResource>();
+            CreateMap<Make, KeyValuePairResource>();
+            CreateMap<Model, KeyValuePairResource>();
+            CreateMap<Feature, KeyValuePairResource>();
             CreateMap<Vehicle, SaveVehicleResource>()
                 .ForMember(vr => vr.Contact, opt => opt.MapFrom(v => new ContactResource { Name = v.ContactName, Email = v.ContactEmail, Phone = v.ContactPhone }))
                 .ForMember(vr => vr.Features, opt => opt.MapFrom( v => v.Features.Select( vf => vf.FeatureId)));
@@ -21,7 +22,7 @@ namespace vega.Mapping
             CreateMap<Vehicle,VehicleResource>()
                 .ForMember(vr => vr.Make, opt => opt.MapFrom(v => v.Model.Make))
                 .ForMember(vr => vr.Contact, opt => opt.MapFrom(v => new ContactResource { Name = v.ContactName, Email = v.ContactEmail, Phone = v.ContactPhone }))
-                .ForMember(vr => vr.Features, opt => opt.MapFrom( v => v.Features.Select( vf => new FeatureResource { Id = vf.FeatureId, Name = vf.Feature.Name, })));
+                .ForMember(vr => vr.Features, opt => opt.MapFrom( v => v.Features.Select( vf => new KeyValuePairResource { Id = vf.FeatureId, Name = vf.Feature.Name, })));
                 
             // Api resource to Domain
             CreateMap<SaveVehicleResource,Vehicle>()
