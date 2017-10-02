0935889557981ceaedf44d16442bee582a8d59c0
Vinay Joshi <vinay01joshi@gmail.com>
2017-10-02 13:04:44 +0530

    Refactoring Towards the Clean Architecture | Fixing some bug

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -55,7 +55,7 @@ namespace vega.Controllers
             vehicle.LastUpdate = DateTime.Now;
 
             await unitOfWork.CompleteAsync();
-
+            vehicle = await repository.GetVehicle(vehicle.Id);
             var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
 
             return Ok(result);
