56977335cb6c1959e6e66c3b4a2cfd8e9594b870
Vinay Joshi <vinay01joshi@gmail.com>
2017-10-10 18:38:53 +0530

    Populating and Submitting Forms | Logging Error only in production envrironment

--- ClientApp/app/app.error-handler.ts
+++ ClientApp/app/app.error-handler.ts
@@ -1,13 +1,18 @@
 import * as Raven from 'raven-js';
 import { ToastyService } from 'ng2-toasty';
-import { ErrorHandler, Inject, NgZone } from '@angular/core';
+import { ErrorHandler, Inject, NgZone , isDevMode } from '@angular/core';
 
 export class AppErrorHandler implements ErrorHandler {
 
     constructor(@Inject(ToastyService) private toastyService: ToastyService) {}
 
     handleError(error: any): void {
-        Raven.captureException(error.originalError || error);
+        if(!isDevMode()){
+            Raven.captureException(error.originalError || error);
+        }else {
+            throw error;
+        }
+
         console.log('Someting went wrong.');
         // this.ngZone.run(()=>{
         //     this.toastyService.error({
