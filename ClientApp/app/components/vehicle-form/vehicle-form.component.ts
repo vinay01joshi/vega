467f2de1b573d84f5e1e045512dfefa6c6c18363
Vinay Joshi <vinjoshi@publicisgroupe.net>
2017-10-12 07:59:06 +0530

    Populating and Submitting Forms | Populating the Forms

--- ClientApp/app/components/vehicle-form/vehicle-form.component.ts
+++ ClientApp/app/components/vehicle-form/vehicle-form.component.ts
@@ -56,8 +56,10 @@ export class VehicleFormComponent implements OnInit {
       Observable.forkJoin(sources).subscribe(data => {
         this.makes = data[0];
         this.features = data[1];
-        if(this.vehicle.id)
+        if(this.vehicle.id){
           this.setVehicle(data[2]);
+          this.populateModels();
+        }
       },err =>{
         if(err.status == 404)
             this.router.navigate(['/']);      
@@ -74,9 +76,14 @@ export class VehicleFormComponent implements OnInit {
   }
   
   onMakeChange() {
+    this.populateModels();
+
+    delete this.vehicle.modelId;
+  }
+
+  private populateModels() {
     var selectedMake = this.makes.find( m => m.id == this.vehicle.makeId );
     this.models = selectedMake ? selectedMake.models : [];
-    delete this.vehicle.modelId;
   }
 
   onFeatureToggle(featureId: number, $event: any) {
