eadc769b5158cf82a2db993ba4b2945fab1835a1
Vinay Joshi <vinjoshi@publicisgroupe.net>
2017-10-12 08:10:54 +0530

    Populating and Submitting forms | Implementing the Update

--- ClientApp/app/components/vehicle-form/vehicle-form.component.ts
+++ ClientApp/app/components/vehicle-form/vehicle-form.component.ts
@@ -96,8 +96,21 @@ export class VehicleFormComponent implements OnInit {
   }
 
   submit() {
-    this.vehicleService.create(this.vehicle)
+    if(this.vehicle.id) {
+      this.vehicleService.update(this.vehicle)
+        .subscribe( x=> {
+          this.toastyService.success({
+            title: 'Success',
+            msg: 'The vehicle was succefully updated.',
+            theme: 'bootstrap',
+            showClose: true,
+            timeout:5000
+          });
+        });
+    }else{
+      this.vehicleService.create(this.vehicle)
       .subscribe( x => console.log(x));
+    }
   }
 
 }
--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -33,12 +33,12 @@ namespace vega.Mapping
                 .ForMember(v => v.Features, opt => opt.Ignore())
                 .AfterMap((vr,v) => {
                     // Remove unselected feature
-                    var removedFeatures = v.Features.Where(f => !vr.Features.Contains(f.FeatureId));                    
+                    var removedFeatures = v.Features.Where(f => !vr.Features.Contains(f.FeatureId)).ToList();                    
                     foreach(var f in removedFeatures)
                         v.Features.Remove(f);
 
                     // Add new Features                    
-                    var addedFeatures = vr.Features.Where(id => !v.Features.Any(f => f.FeatureId == id)).Select(id => new VehicleFeature { FeatureId = id });
+                    var addedFeatures = vr.Features.Where(id => !v.Features.Any(f => f.FeatureId == id)).Select(id => new VehicleFeature { FeatureId = id }).ToList();
                     foreach(var f in addedFeatures)
                          v.Features.Add(f);
                 });
