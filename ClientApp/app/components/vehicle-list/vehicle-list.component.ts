274997aad1e8adc04d1052083f4a834038edb70e
Vinay Joshi <vinay01joshi@gmail.com>
2017-10-23 19:58:04 +0530

    Filtering Sorting and Pagination | Displaying the list of Vehicles and Adding Filter

--- ClientApp/app/components/vehicle-list/vehicle-list.component.ts
+++ ClientApp/app/components/vehicle-list/vehicle-list.component.ts
@@ -1,5 +1,5 @@
 import { VehicleService } from './../../services/vehicle.service';
-import { Vehicle } from './../../models/vehicle';
+import { Vehicle, KeyValuePair } from './../../models/vehicle';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -9,12 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VehicleListComponent implements OnInit {
   vehicles: Vehicle[];
+  makes: KeyValuePair[];
+  filter: any ={};
 
   constructor(private vehicleService: VehicleService) { }
 
   ngOnInit() {
+    this.vehicleService.getMakes()
+      .subscribe(makes => this.makes = makes);
+
     this.vehicleService.getVehicles()
       .subscribe(vehicles => this.vehicles = vehicles);
+  }
+
+  onFilterChange() {
+   
   }
 
 }
